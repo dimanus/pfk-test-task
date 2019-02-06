de7038259abed208737c08c660141d91b1bb135b
Dmitry Strizhov <2926587@gmail.com>
2019-02-06 13:55:06 +0700

    New docker config

--- web/config/console.php
+++ web/config/console.php
@@ -1,32 +1,30 @@
 <?php
-
 $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
-
 $config = [
-    'id' => 'basic-console',
-    'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
+    'id'                  => 'basic-console',
+    'basePath'            => dirname(__DIR__),
+    'bootstrap'           => ['log'],
     'controllerNamespace' => 'app\commands',
-    'aliases' => [
+    'aliases'             => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
-    'components' => [
+    'components'          => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
-        'log' => [
+        'log'   => [
             'targets' => [
                 [
-                    'class' => 'yii\log\FileTarget',
+                    'class'  => 'yii\log\FileTarget',
                     'levels' => ['error', 'warning'],
                 ],
             ],
         ],
-        'db' => $db,
+        'db'    => $db,
     ],
-    'params' => $params,
+    'params'              => $params,
     /*
     'controllerMap' => [
         'fixture' => [ // Fixture generation command line.
@@ -35,7 +33,6 @@ $config = [
     ],
     */
 ];
-
 if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'gii';
